4ae49791da9824b6eb388e5c9740a0ae4cb74199
TeanJD <130848140+TeanJD@users.noreply.github.com>
2024-01-04 23:50:05 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add event listeners and functions for generating PDF
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    // Add code here to generate and download the PDF
+});
